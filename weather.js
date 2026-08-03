578c7b8cf72c8b57235a0d861482aa5bd2e2c586
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-08-03 11:36:26 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,8 +5,8 @@ async function getCanada(lat, lon)
     let params = new URLSearchParams({"f": "json", "bbox": bbox});
     let request = await fetch(`https://api.weather.gc.ca/collections/citypageweather-realtime/items?${params}`);
     let response = await request.json();
-    console.log(features);
-    let weather = response["features"];
+    console.log(response["features"][0]);
+    let weather = response["features"][0];
     
     let current_temperature = weather["properties"]["currentConditions"]["temperature"]["value"] + " C";
     let current_forecast = weather["currentConditions"]["condition"]["en"];
