946f273b65420804cd2082aff4b6837996a61490
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-08-04 10:17:26 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -8,7 +8,7 @@ async function getCanada(lat, lon)
     let weather = response["features"][0]["properties"];
     
     let current_temperature = weather["currentConditions"]["temperature"]["value"]["en"] + " C";
-    let current_forecast = weather["currentConditions"]["condition"]["en"];
+    let current_forecast = weather["currentConditions"]["condition"]["value"]["en"];
     let current_wind = weather["currentConditions"]["wind"]["speed"]["value"]["en"] + " km/h " + weather["currentConditions"]["wind"]["direction"]["value"]["en"];
     weather = "<br>Current temperature:  " + current_temperature + "<br>" +  "Current forecast:  " + current_forecast + "<br>" +  "Current wind:  " + current_wind;
     return weather;
